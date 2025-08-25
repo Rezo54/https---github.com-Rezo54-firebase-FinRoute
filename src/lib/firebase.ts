c402944d0f9b1aedd78177ae3c14461891010b4c
Benedict Mahlangu <benedictmahlangu09@gmail.com>
2025-08-25 12:10:09 +0000

    from firebase settings:

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -5,13 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: "finroute-49u64",
-  appId: "1:435728537189:web:8b6f491d095458e2ccd88c",
-  storageBucket: "finroute-49u64.appspot.com",
   apiKey: "AIzaSyCsuS0nDiCqPPFoQkTRU7Cs3BSDHfvTOTY",
   authDomain: "finroute-49u64.firebaseapp.com",
+  projectId: "finroute-49u64",
+  storageBucket: "finroute-49u64.firebasestorage.app",
   messagingSenderId: "435728537189",
-  measurementId: "G-XXXXXXXXXX",
+  appId: "1:435728537189:web:324058f04ce4fd37ccd88c",
 };
 
 // Initialize Firebase
